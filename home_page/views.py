dc8683519a6d209de23ce9e6dcf40fde59d57868
parides55 <andreasparides5@gmail.com>
2025-07-01 17:07:16 +0100

    create index.html and render it in views.py

--- home_page/views.py
+++ home_page/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. NET DIVERS Home Page works!")
+    """
+    Render the home page of the NET DIVERS application.
+    """
+    return render(request, 'home_page/index.html')
