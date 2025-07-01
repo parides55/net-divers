2e00f3c6e44a0fed798a220e5db85bb482eb8d13
parides55 <andreasparides5@gmail.com>
2025-07-01 15:24:50 +0100

    update settings.py to include heroku to allowed hosts and add the home_page app

--- net_divers/settings.py
+++ net_divers/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yaz)$fxs89v#@(d_d3^gs6mm$uwviq376ewi@5tvihl^dot&v6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home_page',
 ]
 
 MIDDLEWARE = [
