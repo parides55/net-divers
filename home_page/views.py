70b966d27326d123496760adcb2221406072f2a3
parides55 <andreasparides5@gmail.com>
2025-07-02 18:42:36 +0300

    build, style and connect the secondary html page

--- home_page/views.py
+++ home_page/views.py
@@ -6,3 +6,10 @@ def home(request):
     Render the home page of the NET DIVERS application.
     """
     return render(request, 'home_page/index.html')
+
+
+def secondary(request):
+    """
+    Render the secondary page of the NET DIVERS application.
+    """
+    return render(request, 'home_page/secondary.html')
