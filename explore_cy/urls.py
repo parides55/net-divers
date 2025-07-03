4edf75e1b9959886e109f81010fb56b6368a4213
parides55 <andreasparides5@gmail.com>
2025-07-03 20:39:07 +0300

    create html, views and urls for the diving_sites.html

--- explore_cy/urls.py
+++ explore_cy/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("explore_cy", views.explore_cy, name="explore_cy"),
+    path("diving_sites", views.diving_sites, name="diving_sites"),
 ]
--- explore_cy/views.py
+++ explore_cy/views.py
@@ -6,3 +6,10 @@ def explore_cy(request):
     Render the explore page of the NET DIVERS application.
     """
     return render(request, 'explore_cy/explore_cy.html')
+
+
+def diving_sites(request):
+    """
+    Render the diving sites page of the NET DIVERS application.
+    """
+    return render(request, 'explore_cy/diving_sites.html')
