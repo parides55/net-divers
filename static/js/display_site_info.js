93e84190a6dbe96b17df0244b128f28c5f017f1b
parides55 <andreasparides5@gmail.com>
2025-09-17 00:06:15 +0300

    add structure and styles to the sites_info.html

--- static/js/display_site_info.js
+++ static/js/display_site_info.js
@@ -12,11 +12,24 @@ fetch('data/diving_sites.json')
 function displaySiteInfo(site) {
     const container = document.querySelector(".diving-site-col");
     container.innerHTML = `
-        <img src="${site.image}" class="img-fluid mb-4" alt="Image of ${site.name}">
-        <h2>${site.name}</h2>
-        <p><strong>Location:</strong> ${site.location}</p>
-        <p><strong>Depth:</strong> ${site.depth}</p>
-        <p>${site.description}</p>
-        <a href="/diving_sites.html" class="btn btn-primary mt-3">Back to Diving Sites</a>
+        <div class="card text-center">
+            <div class="card-body">
+                <h5 class="card-title">${site.name}</h5>
+                <p class="card-text mt-3">${site.description}</p>
+                <p class="card-text">
+                    <ul class="text-start">
+                        <li><strong>Location:</strong> ${site.location}</li>
+                        <li><strong>Maximum depth:</strong> ${site.depth}</li>
+                        <li><strong>Certification required:</strong> ${site.certification}</li>
+                    </ul>
+                </p>
+            </div>
+            <div class="card-header">
+                <img src="${site.image}" class="card-img-top py-5 course-info-image" alt="${site.title}">
+            </div>
+            <div class="card-footer text-body-secondary">
+                <a href="/diving_sites.html" class="btn btn-outline-dark" >Back to diving sites</a>
+            </div>
+        </div>
     `;
 };
