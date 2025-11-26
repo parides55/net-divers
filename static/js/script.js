ab3b53dec4d4795c244fff89480d373b1c3a30d0
parides55 <andreasparides5@gmail.com>
2025-11-27 00:14:12 +0200

    add 2 additional sections in the homepage, make them responsive and use js to display most popular courses

---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+fetch('data/courses_services.json')
+    .then(response => response.json())
+    .then(data => {
+        printPopularCourses(data);
+    })
+    .catch(error => {
+        console.error('Error fetching JSON:', error);
+    });
+
+function printPopularCourses(data) {
+    const popularCoursesRow = document.getElementById("popular-courses-row");
+
+    data.forEach(course => {
+        if (course.home_page === "yes") {
+            const col = document.createElement("div");
+            col.classList.add("col");
+            col.classList.add("card-cols")
+            
+            col.innerHTML = `
+            <div class="card popular-cards">
+                <img src="${course.image}" class="card-img-top popular-card-img-top" alt="${course.title}">
+                <div class="card-body popular-card-body">
+                    <h4 class="card-title fw-bold">${course.title}</h4>
+                    <a href="course_info.html?id=${course.title}" class="btn btn-outline-primary">View this course</a>
+                </div>
+            </div>
+            `;
+            popularCoursesRow.appendChild(col);
+        }
+    });
+};
