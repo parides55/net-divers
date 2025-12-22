243dadbed1c8a0f63256bebe49c8009a0a0b1660
parides55 <andreasparides5@gmail.com>
2025-12-23 00:34:19 +0200

    remove the text-bg-dark class

--- static/js/display_diving_sites.js
+++ static/js/display_diving_sites.js
@@ -17,10 +17,10 @@ function displayDivingSites(data) {
 
         col.innerHTML = `
             <a href="diving_site_info.html?id=${site.name}" class="text-decoration-none">
-                <div class="card sites-cards text-bg-dark">
+                <div class="card sites-cards">
                     <img src="${site.image}" class="card-img" alt="Image of ${site.name}">
                     <div class="card-img-overlay d-flex align-items-end">
-                        <h3 class="card-title fs-1">${site.name}</h3>
+                        <h3 class="card-title text-light fs-1">${site.name}</h3>
                     </div>
                 </div>
             </a>
