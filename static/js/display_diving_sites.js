e22c186a64a2e0f76e8dd8408afa609427e27640
parides55 <andreasparides5@gmail.com>
2025-12-23 00:12:15 +0200

    change the layout of card display in diving sites html

--- static/js/display_diving_sites.js
+++ static/js/display_diving_sites.js
@@ -19,10 +19,8 @@ function displayDivingSites(data) {
             <a href="diving_site_info.html?id=${site.name}" class="text-decoration-none">
                 <div class="card sites-cards text-bg-dark">
                     <img src="${site.image}" class="card-img" alt="Image of ${site.name}">
-                    <div class="card-img-overlay">
-                        <h5 class="card-title">${site.name}</h5>
-                        <p class="card-text">Location: ${site.location}</p>
-                        <p class="card-text"><small>Depth: ${site.depth}</small></p>
+                    <div class="card-img-overlay d-flex align-items-end">
+                        <h3 class="card-title fs-1">${site.name}</h3>
                     </div>
                 </div>
             </a>
