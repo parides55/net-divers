bcc9250db80777d1b5867c47d05e34059dae80b3
Andreas Parides <parides5@hotmail.com>
2026-01-22 00:04:00 +0200

    add greek content for diving sites info pages and update JS file to detect language

--- static/js/display_diving_sites.js
+++ static/js/display_diving_sites.js
@@ -6,7 +6,7 @@ if (currentFile === 'explore_cy_gr.html') {
     fetch('data/diving_sites_gr.json')
         .then(response => response.json())
         .then(data => {
-            displayDivingSites(data);
+            displayDivingSitesGR(data);
         })
         .catch(error => {
             console.error('Error fetching JSON:', error);
@@ -14,16 +14,16 @@ if (currentFile === 'explore_cy_gr.html') {
 } else {
     // English version
     fetch('data/diving_sites_en.json')
-    .then(response => response.json())
-    .then(data => {
-        displayDivingSites(data);
-    })
-    .catch(error => {
-        console.error('Error fetching JSON:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            displayDivingSitesEN(data);
+        })
+        .catch(error => {
+            console.error('Error fetching JSON:', error);
+        });
 }
 
-function displayDivingSites(data) {
+function displayDivingSitesEN(data) {
     const row = document.querySelector(".sites-row"); // container row
 
     data.forEach(site => {
@@ -32,7 +32,29 @@ function displayDivingSites(data) {
         col.classList.add("mb-5");
 
         col.innerHTML = `
-            <a href="diving_site_info.html?id=${site.name}" class="text-decoration-none">
+            <a href="diving_site_info_en.html?id=${site.name}" class="text-decoration-none">
+                <div class="card sites-cards">
+                    <img src="${site.image}" class="card-img" alt="Image of ${site.name}">
+                    <div class="card-img-overlay d-flex align-items-end">
+                        <h3 class="card-title text-light fs-1">${site.name}</h3>
+                    </div>
+                </div>
+            </a>
+        `;
+        row.appendChild(col);
+    });
+};
+
+function displayDivingSitesGR(data) {
+    const row = document.querySelector(".sites-row"); // container row
+
+    data.forEach(site => {
+        const col = document.createElement("div");
+        col.classList.add("col", "d-flex", "justify-content-center");
+        col.classList.add("mb-5");
+
+        col.innerHTML = `
+            <a href="diving_site_info_gr.html?id=${site.name}" class="text-decoration-none">
                 <div class="card sites-cards">
                     <img src="${site.image}" class="card-img" alt="Image of ${site.name}">
                     <div class="card-img-overlay d-flex align-items-end">
--- static/js/display_site_info.js
+++ static/js/display_site_info.js
@@ -1,15 +1,30 @@
+const currentPath = window.location.pathname;
 const params = new URLSearchParams(window.location.search);
 const siteId = params.get("id");
 
-fetch('data/diving_sites_en.json')
-    .then(response => response.json())
-    .then(data => {
-        const site = data.find(item => item.name === siteId); // pick the right one
-        displaySiteInfo(site);
-    })
-    .catch(error => console.error("Error fetching JSON:", error));
 
-function displaySiteInfo(site) {
+if (currentPath.includes('diving_site_info_gr.html')) {
+    // Greek version
+    fetch('data/diving_sites_gr.json')
+        .then(response => response.json())
+        .then(data => {
+            const site = data.find(item => item.name === siteId); // pick the right one
+            displaySiteInfoGR(site);
+        })
+        .catch(error => console.error("Error fetching JSON:", error));
+} else {
+    // English version
+    fetch('data/diving_sites_en.json')
+        .then(response => response.json())
+        .then(data => {
+            const site = data.find(item => item.name === siteId); // pick the right one
+            displaySiteInfoEN(site);
+        })
+        .catch(error => console.error("Error fetching JSON:", error));
+}
+
+
+function displaySiteInfoEN(site) {
     const container = document.querySelector(".diving-site-col");
     container.innerHTML = `
         <h4 class="text-center mb-4 fst-italic">${site.name}</h4>
@@ -27,7 +42,35 @@ function displaySiteInfo(site) {
         </ul>
     </p>
         <div class="text-body-secondary d-flex justify-content-evenly">
-            <a href="explore_cy.html#diving-sites-section" class="btn btn-outline-dark" >Back to Diving Sites</a>
+            <a href="explore_cy_en.html#diving-sites-section" class="btn btn-outline-dark" >Back to Diving Sites</a>
+        </div>
+    </div>
+    `;
+
+    document.getElementById("diving-site-image-div").src = site.image;
+    document.getElementById("diving-site-image-div").classList.remove("hide");
+    document.getElementById("diving-site-image-div").classList.add("animate__animated", "animate__fadeInRight", "animate__slower");
+};
+
+function displaySiteInfoGR(site) {
+    const container = document.querySelector(".diving-site-col");
+    container.innerHTML = `
+        <h4 class="text-center mb-4 fst-italic">${site.name}</h4>
+    <p>${site.description}</p>
+    <p>
+        <ul>
+            <li><strong>Πόλη:</strong> ${site.city}</li>
+            <li><strong>Τοποθεσία:</strong> ${site.location}</li>
+            <li><strong>Μέγιστο βάθος:</strong> ${site.depth}</li>
+            <li><strong>Απαιτούμενη πιστοποίηση:</strong> ${site.certification}</li>
+            <li><strong>Είδη θαλάσσιας ζωής:</strong> ${site.marine_life}</li>
+            <li><strong>Τύπος καταδύσεων:</strong> ${site.dive_type}</li>
+            <li><strong>Πρόσβαση:</strong> ${site.access}</li>
+            <li><strong>Συμβουλές:</strong> ${site.tips}</li>
+        </ul>
+    </p>
+        <div class="text-body-secondary d-flex justify-content-evenly">
+            <a href="explore_cy_gr.html#diving-sites-section" class="btn btn-outline-dark" >Επιστροφή στα Καταδυτικά Σημεία</a>
         </div>
     </div>
     `;
