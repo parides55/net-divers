7a7a6e2390bec4918ac61c5264cb40992cdaf4f0
Andreas Parides <parides5@hotmail.com>
2026-04-14 22:02:44 +0300

    add a scroll to the top function

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,26 @@
 const currentPath = window.location.pathname;
 const currentFile = currentPath.split('/').pop(); // Get just the filename
 
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 if (currentFile === 'index.html' || currentFile === '') {
     // Greek version
     fetch('data/courses_services_gr.json')
