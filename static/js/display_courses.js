71501c8bcffc9c3617a3d1d3ab488e2528c48bcb
Andreas Parides <parides5@hotmail.com>
2026-04-20 01:35:46 +0300

    add the scroll to top button and update js files with the code

--- static/js/display_courses.js
+++ static/js/display_courses.js
@@ -1,6 +1,25 @@
 const currentPath = window.location.pathname;
 const currentFile = currentPath.split('/').pop(); // Get just the filename
 
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 if (currentFile === 'courses_and_services_gr.html') {
     // Greek version
